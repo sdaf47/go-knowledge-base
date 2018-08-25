75f95ad806768773c331a1f2f3c614fb42fccff4
sdaf47 <sdaf47@yandex.ru>
2018-08-25 15:11:18 +0300

    DataBase service: json database tests

---
+++ small_programms/database/json_test.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"testing"
+	"fmt"
+	"os"
+)
+
+type TestField2 struct {
+	Name string `json:"name"`
+}
+
+var jsonDataBase DataBase
+
+func TestMain(m *testing.M) {
+	var err error
+	jsonDataBase, err = NewJsonDataBase("./store/")
+	if err != nil {
+		panic(err)
+	}
+	retCode := m.Run()
+
+	os.Exit(retCode)
+}
+
+func TestDataJsonBase_Create(t *testing.T) {
+	var err error
+
+	_, err = jsonDataBase.Create(TestField2{Name: "test_11"})
+	if err != nil {
+		t.Fatalf("error during created: %s", err)
+	}
+
+	_, err = jsonDataBase.Create(TestField2{Name: "test_22"})
+	if err != nil {
+		t.Fatalf("error during created: %s", err)
+	}
+
+	res, err := jsonDataBase.Get(TestField2{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	entities := res.([]TestField2)
+	if len(entities) < 1 {
+		t.Fatalf("Get did not return entities")
+	}
+
+	for i, en := range entities {
+		fmt.Println("en", en)
+		en.Name = en.Name + "_updated"
+		err = jsonDataBase.Update(i, en)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
